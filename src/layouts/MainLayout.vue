e7892b6787e2fe4eae338ec68bd84bc7cec653a2
rafael almeida <rafaelalmeidalara007@gmail.com>
2025-01-02 18:18:34 -0400

    wip

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,12 +1,12 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
+    <!-- <q-header elevated>
       <q-toolbar>
         <q-toolbar-title> Mensagem ou Marca Aqui </q-toolbar-title>
 
         <div>Davi V1</div>
       </q-toolbar>
-    </q-header>
+    </q-header> -->
 
     <q-page-container>
       <router-view />
